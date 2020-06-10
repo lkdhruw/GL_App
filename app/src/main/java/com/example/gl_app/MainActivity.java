771dcbc6a0f5932a728f8a9869ed081d5f279f8a
Laxmi Kant Dhruw <lkdhruw@live.com>
2020-06-10 09:50:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gl_app/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.gl_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GLSurfaceView gLView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        gLView = new MyGLSurfaceView(this);
+        setContentView(gLView);
+    }
+}
